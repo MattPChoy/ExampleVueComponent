36f27ec08a8dad155bc67699e9afc71dde8be33e
MattPChoy <matthewpchoy@gmail.com>
2025-07-08 20:11:35 +1000

    publishing d.ts

---
+++ npm-package/src/index.ts
@@ -0,0 +1,5 @@
+/**
+ * This file defines the exports for the npm package.
+ */
+
+export { default as HelloWorld } from './components/HelloWorld.vue';
--- npm-package/vite.config.ts
+++ npm-package/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from "vite-plugin-dts";
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: "test-package",
+      //A function that generates the output file name for different formats during the build
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    sourcemap: true,
+  },
+  plugins: [vue(), dts()],
 })
