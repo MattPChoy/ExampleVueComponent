ecaa843a364b7a0df280ee96107dc15bb741d8a7
MattPChoy <matthewpchoy@gmail.com>
2025-07-11 22:59:13 +1000

    Cleanup

--- my-package/vite.config.ts
+++ my-package/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
+      entry: resolve(__dirname, 'src/lib.ts'),
       name: "my-package",
       formats: ['es', 'umd'],
       fileName: (format) => `main.${format}.js` // <-- This line
